491a71a1d0bd12b5ba88ab61e87b12fc683e23e4
Scrunshes <scrunshes@gmail.com>
2016-02-24 21:03:29 +0100

    chore(project): changed some tests, removed a catch, changed an error throwing

--- src/pagination.js
+++ src/pagination.js
@@ -2,7 +2,7 @@ import {bindable, inject, computedFrom} from 'aurelia-framework';
 import {customElement} from 'aurelia-templating';
 import {EntityManager} from 'spoonx/aurelia-orm';
 
-@inject(EntityManager)
+@inject(EntityManager, Element)
 @customElement('pagination')
 export class Pagination {
   @bindable page;
@@ -11,34 +11,60 @@ export class Pagination {
 
   @bindable count = 0;
 
-  @bindable resource = null;
-
   @bindable pagechange = null;
 
+  /**
+   * Calculate number of pages whenever 'count' or 'limit' is updated.
+   *
+   * @returns {number} page count
+   */
   @computedFrom('count', 'limit')
   get pageCount () {
     return Math.ceil(this.count / this.limit);
   }
 
-  constructor (entityManager) {
+  /**
+   * Create a new pagination element.
+   *
+   * @param {Entity} entityManager
+   * @param {Element} element
+   */
+  constructor (entityManager, element) {
     this.entityManager = entityManager;
+    this.element = element;
   }
 
+  /**
+   * Get resource, if exists initiate repository and update count.
+   */
   attached () {
-    if (this.resource) {
-      this.repository = this.entityManager.getRepository(this.resource);
+    this.resource = this.element.getAttribute('resource');
+    if (!this.resource) {
+      return;
     }
+    this.repository = this.entityManager.getRepository(this.resource);
     this.updateRecordCount();
   }
 
+  /**
+   * Load previous page.
+   */
   loadPrevious () {
     this.load(this.page - 1);
   }
 
+  /**
+   * Load next page.
+   */
   loadNext () {
     this.load(this.page + 1);
   }
 
+  /**
+   * Load a page and call the pagechange function bind to the element if exists.
+   *
+   * @param page
+   */
   load (page) {
     if (page === 0) {
       return;
@@ -52,11 +78,8 @@ export class Pagination {
 
     if (typeof this.pagechange === 'function') {
       this.pagechange({page:page})
-        .then(result => {
+        .then(() => {
           this.page = page;
-        })
-        .catch(error => {
-          console.error("Something went wrong.", error);
         });
     }
     else {
@@ -65,13 +88,17 @@ export class Pagination {
 
   }
 
+  /**
+   * Update record count fetching the repository.
+   */
   updateRecordCount () {
-    if (this.resource) {
+    if (this.repository) {
       this.repository.count()
         .then(response => this.count = response.count)
-        .catch(response => console.error(response));
+        .catch(error => {
+          throw new Error(`Error updating count : ${error}`);
+        });
     }
-
   }
 
 }
